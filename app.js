c1815439f93e1dca026ec9eb1d72326a69e4fa9c
Shahzad Khan <shahzadkhan3iii7@gmail.com>
2017-08-14 23:51:22 -0500

    seconds, dst, and leap year done

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ let chalk = require('chalk');
 
 console.log('It is ' + chalk.blue(moment().format("dddd, MMMM Do YYYY, h:mm:ss a")));
 console.log('It is the ' + chalk.magenta(moment().format("DDDo")) + ' day of the year.');
+
+let now = moment();
+let startOfDay = moment();
+startOfDay.startOf('day'); // .startOfDay mutates now
+
+console.log('It is ' + chalk.cyan(now.diff(startOfDay, 'seconds')) + ' seconds into the day.');
+
+let dst = (now.isDST()) ? 'is' : 'is not'
+console.log('It ' + chalk.green(dst) + ' during Daylight Savings Time.');
+let leap = (now.isLeapYear()) ? 'is' : 'is not'
+console.log('It ' + chalk.red(leap) + ' a leap year.');
