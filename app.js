c76709e8a7f10c3c7cf4fc2a4ebcb2084ee08fc2
Shahzad Khan <shahzadkhan3iii7@gmail.com>
2017-08-14 23:50:41 -0500

    current time and day of the year done

---
+++ app.js
@@ -0,0 +1,5 @@
+let moment = require('moment');
+let chalk = require('chalk');
+
+console.log('It is ' + chalk.blue(moment().format("dddd, MMMM Do YYYY, h:mm:ss a")));
+console.log('It is the ' + chalk.magenta(moment().format("DDDo")) + ' day of the year.');
